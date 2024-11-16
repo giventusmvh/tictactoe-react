be6acaa502baf4f684fb3a06af5270e8fbc9bf74
Giventus <weishen735@gmail.com>
2024-11-16 21:32:26 +0700

    init daaaa

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    return setIsEditing((bool) => !bool);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} required onChange={handleChange} />
+    );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
+}
